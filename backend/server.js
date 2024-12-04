aa07d9577d7e88cce70f36a28b0bb40a04912a25
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-04 20:34:13 -0300

    refactor(backend): use PORT env variable

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config(); // Load environment variables
 
-dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log('Server started at http://localhost:5000');
 });
