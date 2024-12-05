847d65035a3826e60dfc57d745fd96eb2bd53908
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-05 11:09:50 -0300

    feat(backend): add getAllStudents controller

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,16 @@
+import getAllStudentsService from '../services/studentService';
+
+const getAllStudentsController = async (req, res, next) => {
+  try {
+    const data = await getAllStudentsService();
+
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default getAllStudentsController;
