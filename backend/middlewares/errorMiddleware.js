057a0b7170a11aba0040be7d2ff7e3f94ed545bf
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-05 19:52:13 -0300

    refactor(backend): import status code for consistency (errorMiddleware)

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,11 +1,10 @@
+import { INTERNAL_SERVER_ERROR } from '../utils/statusCodes.js';
+
 // eslint-disable-next-line no-unused-vars
 const errorMiddleware = (err, _req, res, _next) => {
-  console.error(err.stack);
-
-  const statusCode = err.status || 500;
   const message = err.message || 'Internal Server Error';
 
-  res.status(statusCode).json({
+  res.status(INTERNAL_SERVER_ERROR).json({
     success: false,
     message,
     error: err.stack,
