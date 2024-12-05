533e3529d7a1f935b895e31d42612529db6f7aca
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-05 01:52:06 -0300

    feat(frontend): add proxy for API requests to backend server (Vite)
    - This enables seamless communication between frontend and backend during development, avoiding CORS issues.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+      },
+    },
+  },
   plugins: [vue(), eslint()],
 });
