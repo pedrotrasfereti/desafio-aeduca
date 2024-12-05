58bd2bbb73ecce77b786ee38ec373e4aceb49494
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-05 19:29:14 -0300

    feat(backend): add loginUser method to auth service

---
+++ backend/services/authService.js
@@ -0,0 +1,20 @@
+import * as roleModel from '../models/roleModel.js';
+import * as userModel from '../models/userModel.js';
+import { BAD_REQUEST, UNAUTHORIZED } from '../utils/statusCodes.js';
+import ValidationError from '../utils/validationError.js';
+
+export const loginUser = async (login, password) => {
+  if (!login || !password) {
+    throw new ValidationError('Login e senha são obrigatórios', BAD_REQUEST);
+  }
+
+  const user = await userModel.getUserByLogin(login);
+
+  if (!user || user.password !== password) {
+    throw new ValidationError('Login ou senha inválidos', UNAUTHORIZED);
+  }
+
+  const role = await roleModel.getRoleById(user.role_id);
+
+  return { login: user.login, role: role.role };
+};
