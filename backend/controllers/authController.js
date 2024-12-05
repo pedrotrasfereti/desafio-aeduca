3287e762ad30e84600664ec0906d4db05b8a134a
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-05 19:38:45 -0300

    feat(backend): add login method to auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,14 @@
+import authService from '../services/authService.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import { OK } from '../utils/statusCodes.js';
+
+export const login = asyncHandler(async (req, res) => {
+  const { login, password } = req.body;
+
+  const userData = await authService.loginUser(login, password);
+
+  res.status(OK).json({
+    success: true,
+    data: userData,
+  });
+});
