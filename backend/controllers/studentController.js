82d5b99f99dd5d9a403ca323fa1ccc0a8bd1de5c
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-05 16:17:08 -0300

    feat(backend): add success message to response json (studentController)

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,10 +1,10 @@
 import * as studentService from '../services/studentService.js';
 import asyncHandler from '../utils/asyncHandler.js';
-import { OK, CREATED, NO_CONTENT } from '../utils/statusCodes.js';
+import { OK, CREATED } from '../utils/statusCodes.js';
 
 export const getAllStudents = asyncHandler(async (_req, res) => {
   const data = await studentService.getAllStudents();
-  res.status(OK).json({ success: true, data });
+  return res.status(OK).json({ success: true, data });
 });
 
 export const createStudent = asyncHandler(async (req, res) => {
@@ -12,7 +12,9 @@ export const createStudent = asyncHandler(async (req, res) => {
 
   const data = await studentService.createStudent(student);
 
-  res.status(CREATED).json({ success: true, data });
+  return res
+    .status(CREATED)
+    .json({ success: true, data, message: 'Aluno cadastrado com sucesso.' });
 });
 
 export const updateStudent = asyncHandler(async (req, res) => {
@@ -21,7 +23,9 @@ export const updateStudent = asyncHandler(async (req, res) => {
 
   const data = await studentService.updateStudent(ra, student);
 
-  res.status(OK).json({ success: true, data });
+  return res
+    .status(OK)
+    .json({ success: true, data, message: 'Cadastro atualizado com sucesso.' });
 });
 
 export const deleteStudent = asyncHandler(async (req, res) => {
@@ -29,5 +33,7 @@ export const deleteStudent = asyncHandler(async (req, res) => {
 
   await studentService.deleteStudent(ra);
 
-  res.status(NO_CONTENT).json({ success: true });
+  return res
+    .status(OK)
+    .json({ success: true, message: 'Cadastro deletado com sucesso.' });
 });
