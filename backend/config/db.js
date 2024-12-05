63e115d9c5e74b4fb0e70b3c2b476dcb94505ae9
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-04 21:36:57 -0300

    feat(backend): create db connection pool

---
+++ backend/config/db.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+dotenv.config(); // Load environment variables
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  // Ensure persistent connection
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  queueLimit: 0,
+  connectTimeout: 30000,
+  idleTimeout: 60000,
+});
+
+// Test connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+    process.exit(1);
+  }
+
+  console.log('Connected to the database.');
+  connection.release();
+});
+
+export default pool;
