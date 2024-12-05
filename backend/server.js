0251ae42fd7c290ed753f0bf1b3f99e768058507
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-04 21:38:31 -0300

    feat(backend): test mysql pool connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import pool from './config/db.js';
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  try {
+    const [results] = await pool.query('SELECT * FROM students');
+    res.json(results);
+  } catch (err) {
+    console.error('Error fetching data:', err.message);
+    res.status(500).json({ error: 'Database query error' });
+  }
 });
 
 app.listen(PORT, () => {
