ddc531185f058f5bebe6cc38cb63182d7accb64e
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-05 21:08:39 -0300

    feat(backend): create Joi userSchema for validation

---
+++ backend/utils/joi/userSchema.js
@@ -0,0 +1,26 @@
+import Joi from 'joi';
+
+const studentSchema = Joi.object({
+  login: Joi.string().min(3).required().messages({
+    'string.base': 'O campo "Login" deve ser uma string',
+    'string.empty': 'O campo "Login" não pode ficar vázio',
+    'string.length': 'O campo "Login" deve ter no mínimo 3 letras',
+    'any.required': 'O campo "Login" é obrigatório',
+  }),
+
+  password: Joi.string().min(8).required().messages({
+    'string.base': 'O campo "Senha" deve ser uma string',
+    'string.empty': 'O campo "Senha" não pode ficar vázio',
+    'string.length': 'O campo "Senha" deve ter no mínimo 8 letras',
+    'any.required': 'O campo "Senha" é obrigatório',
+  }),
+
+  roleId: Joi.number().valid(0, 1).required().messages({
+    'any.only': 'Nível de Permissão inválido',
+    'string.base': 'O campo "Nível de Permissão" deve ser um número',
+    'string.empty': 'O campo "Nível de Permissão" não pode ficar vázio',
+    'any.required': 'O campo "Nível de Permissão" é obrigatório',
+  }),
+});
+
+export default studentSchema;
