872538c5d5a75cde4dc54a6e667d86b7dcefc8ba
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-06 14:34:22 -0300

    refactor(backend): re-export middlewares in index.js file

---
+++ backend/middlewares/index.js
@@ -0,0 +1,3 @@
+export { default as authenticateJWT } from './authenticateJWT.js';
+export { default as authorizeAdmin } from './authorizeAdmin.js';
+export { default as errorMiddleware } from './errorMiddleware.js';
