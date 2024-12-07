8d16f4a8d20c531cd3b4c66f8bdee4e73ad00bad
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-07 20:39:24 -0300

    feat(frontend): guard /dashboard route for extra security
    Check if user is authenticated before entering dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,17 +3,41 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import DashboardPage from '../pages/DashboardPage.vue';
 import LoginPage from '../pages/LoginPage.vue';
+import useUserStore from '../stores/userStore';
+
+const authGuard = (_to, _from, next) => {
+  const userStore = useUserStore();
+
+  if (userStore.isAuthenticated) {
+    next();
+  } else {
+    next('/');
+  }
+};
 
 const routes = [
   {
     path: '/',
     name: 'Login',
     component: LoginPage,
+    beforeEnter: (to, from, next) => {
+      const userStore = useUserStore();
+      userStore.initializeAuth();
+
+      if (userStore.isAuthenticated) {
+        // Redirect authenticated user to dashboard
+        next('/dashboard');
+      } else {
+        // Allow unauthenticated user to access the login page
+        next();
+      }
+    },
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: DashboardPage,
+    beforeEnter: authGuard,
   },
 ];
 
