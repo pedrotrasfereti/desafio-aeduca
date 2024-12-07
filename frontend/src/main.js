c2d90d3ed5dec82d6e095570745e244e43a97b48
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-12-07 11:55:17 -0300

    feat(frontend): set up pinia to centralize app's state
    - Install pinia for state management
    - Install axios to manage API requests

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import registerPlugins from './plugins';
 
-// Components
-
-// Composables
-
 const app = createApp(App);
 
 registerPlugins(app);
--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -1,9 +1,10 @@
 // Plugins
 import vuetify from './vuetify';
 import router from '../router';
+import pinia from '../stores';
 
 const registerPlugins = (app) => {
-  app.use(vuetify).use(router);
+  app.use(vuetify).use(router).use(pinia);
 };
 
 export default registerPlugins;
